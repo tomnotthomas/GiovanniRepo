fdd49af6f6ef206367528a210258f9c1448080cd
Giovanni Silvotti <giopr.silvotti@yahoo.com>
2023-11-15 13:13:06 +0100

    Finish Tree, with recursive version of contains

--- scripts/tree.js
+++ scripts/tree.js
@@ -1,7 +1,30 @@
 'use strict';
 
 function Tree (value) {
-
+  this.value = value;
+  this.children = [];
 }
+
+Tree.prototype.addChild = function (node) {
+  this.children.push(node);
+  return true;
+};
+
+// Recursive Version
+Tree.prototype.contains = function (value) {
+  
+  // If value is found in this node return true
+  if (this.value === value) return true;
+  console.log(this.children);
+
+  // Else loop over this node's children
+  for (let child of this.children) {
+    // If recursive call on child is true then return true
+    if (child.contains(value)) return true;
+  }
+
+  // If value hasn't been found return false
+  return false;
+};
 
 module.exports = Tree;
