c7fe3cfb36d6a65c96541132023116e4a060a612
Giovanni Silvotti <giopr.silvotti@yahoo.com>
2023-11-15 12:31:38 +0100

    Finish Set

--- scripts/set.js
+++ scripts/set.js
@@ -1,5 +1,23 @@
 'use strict';
 
-function Set () {}
+function Set () {
+  this.storage = {};
+  this.count = 0;
+}
+
+Set.prototype.add = function (value) {
+  if (!this.contains(value))
+    {this.storage[this.count] = value; this.count++; return true};
+};
+
+Set.prototype.contains = function (value) {
+  return Object.values(this.storage).includes(value);
+};
+
+Set.prototype.remove = function (value) {
+
+  delete this.storage[Object.entries(this.storage).findIndex(element=> element[1]===value)];
+  return true;
+};
 
 module.exports = Set;
