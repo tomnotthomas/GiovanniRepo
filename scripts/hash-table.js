dff9c57c85ae395955a5d2731ba9bfd0677ef1e4
Giovanni Silvotti <giopr.silvotti@yahoo.com>
2023-11-16 12:35:14 +0100

    Add working insert and retrieve Methods

--- scripts/hash-table.js
+++ scripts/hash-table.js
@@ -9,7 +9,67 @@ const Storage = helpers.Storage;
 const hash = helpers.hash;
 
 function HashTable (size) {
-
+  this.storage = new Storage(size);
+  this.size = size;
 }
+
+HashTable.prototype.insert = function (key, value) {
+  let node = this.storage.get(hash(key, this.size));
+
+  if (node) {
+
+    if (key === node.key) {
+      node.value = value;
+      return true;
+    }
+
+    while (node.next) {
+      node = node.next;
+      if (key === node.key) {
+        node.value = value;
+        return true;
+      }
+    }
+
+    node.next = {key, value};
+    return true;
+  } else {
+    this.storage.set(hash(key, this.size), {key, value});
+  }
+  return true;
+};
+
+HashTable.prototype.retrieve = function (key) {
+  let node = this.storage.get(hash(key, this.size));
+
+  while (node) {
+    if (key === node.key) {
+      return node.value;
+    } else {
+      node = node.next;
+    }
+  }
+  return undefined;
+};
+
+HashTable.prototype.remove = function (key) {
+  // let node = this.storage.get(hash(key, this.size));
+
+  // while (node) {
+  //   if (key === node.key) {
+  //     return node.value;
+  //   } else {
+  //     node = node.next;
+  //   }
+  // }
+  // return undefined;
+};
+
+
+console.log(hash('hello', 2));
+console.log(hash('world', 2));
+
+const obj = {};
+console.log(obj.value);
 
 module.exports = HashTable;
